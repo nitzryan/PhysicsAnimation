0314ffcb21daa3c98c9e45774b3381c7b8d95f6f
Jason Van Beusekom <vanbe095@umn.edu>
2023-10-17 11:29:10 -0500

    scene boiler plate

---
+++ PhysicsAnimation/Scene.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Rope.h"
+#include "rendering/IRenderable.h"
+
+class Scene {
+public:
+	Scene();
+	~Scene();
+	Rope ropes[];
+	bool update(float dt);
+	std::vector<IRenderable>GetRender();
+private:
+
+};
+
